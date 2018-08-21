7851aa7097ee8475b2e14d544984a48292bd24d6
Will <u3142921@uni.canberra.edu.au>
2018-08-21 17:58:18 +1000

    //basic login page code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form>
+           <label>
+              Email:
+              <input type="text" name="name" />
+           </label>
+           <label>
+              Password:
+              <input type="text" name="Password"/>
+           </label>
+           <input type="submit" value="Submit" />
+        </form>
+      <input type="submit" value="Register"/>
       </div>
     );
   }
 }
+
+
 
 export default App;
