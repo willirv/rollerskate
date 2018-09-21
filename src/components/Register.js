2a2ca2a8338a4ecd98d81a401d45a5536b0cdce5
Will <u3142921@uni.canberra.edu.au>
2018-09-21 15:24:05 +1000

    // working on register page

--- src/components/Register.js
+++ src/components/Register.js
@@ -11,52 +11,158 @@ import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 
 export default class Register extends React.Component{
 
-  constructor(props) {
+
+  constructor(props){
     super(props);
-    this.state = {
-      name:"",
-      Age:"",
+// Set the state of the form as email and password --- will be the element we push to firebase database 
+     this.state = {
       email:"",
       password:""
+     };
   }
+
+// Create a valid form state and define the email and password elements 
+  validateForm(){
+     return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+// Set an event for when the input form is changed
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+
+//*********************************** Submit button ***********************************
+//Create a handle for button/ submit 
+  handleSubmit = event => {
+
+
+//Prevent the user submiting the form when there is no password inputted
+    event.preventDefault();
+//Declare the password variable 
+    var password = document.getElementById("password").value;
+//Declare the email variable 
+    var email = document.getElementById("email").value;
+//Console.log the password variable for debug 
+    console.log(password);
+//Console.log the email variable for debug 
+    console.log(email);
+//Set the credential variable - To find whether the user is auth() or not
+    var credential = firebase.auth.EmailAuthProvider.credential(email, password);
+//Log variable for debug 
+    console.log(credential);
+
+//*********************************** If email and password = true ***********************************
+
+//Call firebase auth() function with the inputted email and password
+firebase.auth().signInWithEmailAndPassword( email, password)
+//If the email and password match firebase database call the firebase user function
+   .then(function(User) {
+
+       var user = firebase.auth().currentUser;
+       console.log(user);
+       //Success - the users email and password is on database
+       //Console log for debug
+       console.log("yay you're a user");
+       window.location = "dashboard";
+       //Set the window location to the ./dashboard element
+
+
+   })
+//*********************************** If email and password = false ***********************************
+//If the email and password DO NOT match firebase data run the catch function
+  .catch(function(error) {
+       //Error Handling - The users password does not match firebase
+       //Console log for debug 
+       console.log(firebase.auth().currentUser);
+       //debugging - alert 
+       alert("you are not a user");
+       //debugging - console .log
+       console.log("User not found");
+
+  
+  });
 
 }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
 
-  handleSubmit(event) {
-
-  }
 
   render() {
-    return (
-      <div id="register-content">
-        <div id="register-home">
-            <Link to={"/"}> &larr; back </Link>
+      return(
+         <div id="register">
+           <div id="register-home">
+           <Link to={"/login"}> &larr; back </Link>
             <Link to={"/login"}>HOME</Link>
-        </div>
+           </div>
+      <div id="login-form">
+      <h4>Register</h4>
+
+/* Enter name */
       <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} id="Name" />
-        </label>
-        <label>
-          Age:
-          <input type="text" value={this.state.value} onChange={this.handleChange} id="age" />
-        </label>
-        <label>
-          Email:
-          <input type="text" value={this.state.value} onChange={this.handleChange} id="email" />
-        </label>
-        <label>
-          Password:
-          <input type="text" value={this.state.value} onChange={this.handleChange} id="password" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-    );
+          <FormGroup controlId="name" bsSize="large">
+            <FormControl
+              id="name"
+              autoFocus
+              type="name"
+              placeholder="Enter your name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+
+/* Enter age */        
+             <FormGroup controlId="age" bsSize="large">
+            <FormControl
+              id="age"
+              autoFocus
+              type="age"
+              placeholder="Enter your age"
+              value={this.state.age}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+
+/* Enter email */      
+          <FormGroup controlId="email" bsSize="large">
+            <FormControl
+              id="email"
+              autoFocus
+              type="email"
+              placeholder="Enter your email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+
+/* Enter password */      
+          <FormGroup controlId="password" bsSize="large">
+            <FormControl
+              id="password"
+              placeholder="Enter your password"
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <Button
+            id="login-submit"
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
+           <div id="link-bar">
+            <div className="forgotpassword-button">
+               <Link to={"forgotpassword"}>Forgot your password?</Link>
+            </div>
+            </div>
+           </div>
+        </div>
+      );
   }
 }
