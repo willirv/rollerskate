7a324695a640b7b4e3b19447edcbcd33daad86e1
Will <u3142921@uni.canberra.edu.au>
2018-11-03 12:16:48 +1100

    // Cleaning up the matches - messages pop up

--- src/components/matches.js
+++ src/components/matches.js
@@ -1,23 +1,26 @@
 //Import React from React 
 import React, { Component } from 'react';
-
 //Import firebase from firebase 
 import firebase from "firebase";
-
+//Import Matches stylesheet 
+import styles from '../styles/matches.css';
 //Import react-router elements 
 import { Link, Router, Route, browserHistory } from "react-router"; 
 //Import form elements from react-bootstrap
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-
+//Import Jquery 
 import $ from "jquery";
-
+//Render component
 export default class matches extends React.Component{
 
 // Create a div for each of the matches
   // Append the name of the match 
     // Append the profile image of the match
+      //Create a div within each match with a unique id 
+        // Get the unique I div and hide it potencially
 
 componentDidMount() {
+
     
 // Call the current auth() state of the user
   firebase.auth().onAuthStateChanged((user) => {
@@ -61,16 +64,62 @@ componentDidMount() {
 // Declare the name variable
    var user_name = doc.data().name;
 
+// Declare a variable for the uid of the unique user 
+   var user_uid = doc.data().uid;
+
 // Declare the profile image url of the user as profile_img
    var profile_img = doc.data().profileimg;
 
 // Create a new div element <div>
    var newDiv = document.createElement("div");
 
+// Set the attribute of the div's as the names of the matches 
+   newDiv.setAttribute('value', user_name );
+
+
+// Create a new <p> element
+   var match_name = document.createElement("p");
+
+// Then append the users name to the new <p> element
    var matchContent = document.createTextNode(user_name); 
 
-// Set the id attribute of the div to the users name
-   newDiv.setAttribute('id', user_name);
+// Append the username of the user to the new <p> element
+    match_name.append(matchContent);
+
+
+/************************************************ Timer Component ************************************************/
+
+// Create a new div called time => when the user types in the message screen update the timer
+   var timer = document.createElement("div");
+
+// Create an input 
+   var timer_output = document.createElement("input");
+
+// Set the value of the input to 5000 just for debug
+   timer_output.setAttribute('value', 5000);
+
+// Append the time_output element to the timer div
+   timer.append(timer_output)
+
+/************************************************ Messages_tab ************************************************/
+
+ //For each of the matches that we have 
+  //Create a new div for each called messages => or with each users unique uid
+    //We need to hide the div for the messages tab and transition it when the user selects one of their matches
+     // When we've completed that task, we then need to create a header on the page with the match users name
+       //Create the spearate components of the chat, messages, type your message
+         // And the sumbit field
+
+// Create a new div element for each of the users 
+   var message_tab = document.createElement("div");
+
+// Set the ID of the message tab div as message tab
+   message_tab.setAttribute('id', "messagetab");
+
+// Set the value of the created div as the users unique uid 
+   message_tab.setAttribute('value', user_uid);
+  
+
 
 // Create a new img as display img and set its attributes for debug
    var display_img = new Image(200,200);
@@ -81,11 +130,23 @@ componentDidMount() {
 // Append the name for debug
    //document.getElementById("matches_container").append(user_name);
 
-   newDiv.append(matchContent);
 
+    newDiv.append(match_name);
     newDiv.append(display_img);
+    newDiv.append(timer);
+    newDiv.append(message_tab);
 
     document.getElementById("matches_container").append(newDiv)
+
+
+
+$('#matches_container > div').click(function(){
+
+
+ var test = $(this).attr('value');
+
+ alert(test);
+});
     
 
    });
@@ -96,12 +157,9 @@ componentDidMount() {
 }
 
 });
-
 }
 
-
-
-
+//#matches_container > div:nth-child(2)
 
 render(){
 return(
