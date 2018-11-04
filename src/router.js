63e4203a91cce3cefb78a262fe94a7bd7eff5c88
Will <u3142921@uni.canberra.edu.au>
2018-11-04 11:11:26 +1100

    //clean

--- src/router.js
+++ src/router.js
@@ -9,7 +9,6 @@ import {Router, Route, browserHistory } from "react-router";
 import home from "./components/welcome";
 import register from "./components/Register";
 import login from "./components/Login";
-import fgpassword from "./components/forgotpassword.js"
 import dashboard from "./components/dashboard";
 import upload from "./components/upload";
 import profileimg from "./global/profile_img";
@@ -25,7 +24,6 @@ export default class App extends React.Component{
 			    <Route path={"/"} component={home} />
 			    <Route path={"login"} component={login} />
 			    <Route path={"register"} component={register} />
-			    <Route path={"forgotpassword"} component={fgpassword} />
 			    <Route path={"dashboard"} component={dashboard} />
 			    <Route path={"upload"} component={upload} />
 			    <Route path={"profileimg"} component={profileimg} />
